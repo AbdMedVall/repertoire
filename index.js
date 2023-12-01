b7f5a4de0d8a6c20bb60e1880df0404e5a393c23
AbdMedVall <136504490+AbdMedVall@users.noreply.github.com>
2023-12-01 03:12:08 +0100

    persons

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+app.use(express.static('dist'))
 app.use(cors())
 app.use(morgan('tiny'));
 morgan.token('host', function(req, res) {
